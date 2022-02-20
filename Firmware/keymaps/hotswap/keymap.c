97cf670e03ddf116cffdc8f3f38a169368a43efb
Mingze Jin <mingze.jin11@gmail.com>
2022-02-20 18:59:09 -0500

    Updating PCB and firmware

--- Firmware/keymaps/hotswap/keymap.c
+++ Firmware/keymaps/hotswap/keymap.c
@@ -38,25 +38,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MUTE,
 		/* tab     Q        W        E        R        T        Y        U        I        O        P        [        ]        \      delete */ 
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
-		/* caps    A        S        D        F        G        H        J        K        L        ;        '                enter     up   */
-		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_UP,
-		/* shift            Z        X        C        V        B        N        M        ,        .        /      shift             insert */
-		KC_LSFT,		  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_INS,
-		/* ctrl   win      alt                                space               alt     win      ctrl     fn       left     down     right */
-		KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,           KC_RALT, KC_RGUI, KC_RCTL, MO(FN),  KC_LEFT, KC_DOWN, KC_RGHT),
+		/* caps    A        S        D        F        G        H        J        K        L        ;        '        ~       enter     up   */
+		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_UP,
+		/* shift   \        Z        X        C        V        B        N        M        ,        .        /       shift    print   insert */
+		KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_PSCR, KC_INS,
+		/* ctrl   win      alt               bkspc    enter    space    space     alt      win     ctrl     fn       left     down     right */
+		KC_LCTL, KC_LGUI, KC_LALT,          KC_BSPC, KC_ENT,  KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL, MO(FN),  KC_LEFT, KC_DOWN, KC_RGHT),
 
 	// Function layer
 	[FN] = LAYOUT_hotswap(
 		/* esc     F1       F2       F3       F4       F5       F6       F7       F8       F9       F0       F11      F12     bkspc    sleep */
 		KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_SLEP,
-		/* tab     Q        W        E      reset      T        Y        U        I        O        P        [        ]        \       pg up */ 
+		/* tab     Q        W        E      reset      T        Y        U        I        O       pass      [        ]        \       pg up */ 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, PASS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP,
-		/* caps    A        S        D        F        G        H      easter     K        L        ;        '                enter     up   */
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EASTER,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
-		/* shift            Z        X        C        V        B        N      email      ,        .        /      shift              pg dn */
-		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EMAIL  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_PGDN,
-		/* ctrl   win      alt                                space               alt     win      ctrl     fn       prev     play     next  */
-		KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT)
+		/* caps    A        S        D        F        G        H      easter     K        L        ;        '        ~       enter     up   */
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EASTER,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		/* shift   \        Z        X        C        V        B        N      email      ,        .        /       shift    print    pg dn */
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EMAIL  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN,
+		/* ctrl   win      alt               bkspc    enter    space    space     alt      win     ctrl      fn      prev     play     next  */
+		KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT)
 };
 
 // Encoder function definition
