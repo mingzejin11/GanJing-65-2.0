c6708ed74246c6834b7a67b83be8630fcdfe61b7
Mingze Jin <mingze.jin11@gmail.com>
2022-09-26 14:37:15 -0400

    Idk

---
+++ Firmware/GanJing65/config.h
@@ -0,0 +1,116 @@
+/* Copyright 2021 mingzejin11
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x6B62
+#define PRODUCT_ID      0x6869
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    mingzejin11
+#define PRODUCT         GanJing 70
+#define DESCRIPTION     70% keyboard designed by Mingze Jin
+
+/* Keyboard matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* Keyboard matrix pins */
+#define MATRIX_ROW_PINS { B0, E6, D7, C6, D4 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, F0, F1, C7, D5, B7, B5, B4 }
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+#define USB_MAX_POWER_CONSUMPTION 100
+
+/* Encoder pad definition */
+#define ENCODERS_PAD_A { D2 }
+#define ENCODERS_PAD_B { D3 }
+#define ENCODER_RESOLUTION 4
+
+/* Enable the OLED driver */
+#define OLED_DISPLAY_128X64
+#define OLED_TIMEOUT 300000 // Measured in milliseconds = 5 minutes
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Always sends esc when ctrl is held to enable the task manager shortcut on Windows */
+#define GRAVE_ESC_CTRL_OVERRIDE
+
+/*
+ * Force NKRO
+ *
+ * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
+ * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
+ * makefile for this to work.)
+ *
+ * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
+ * until the next keyboard reset.
+ *
+ * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
+ * fully operational during normal computer usage.
+ *
+ * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
+ * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
+ * bootmagic, NKRO mode will always be enabled until it is toggled again during a
+ * power-up.
+ *
+ */
+//#define FORCE_NKRO
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
+
+/* Bootmagic Lite key configuration */
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+
+// To save on firmware size lol
+#ifndef NO_DEBUG
+    #define NO_DEBUG
+#endif // !NO_DEBUG
+
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+    #define NO_PRINT
+#endif // !NO_PRINT
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
---
+++ Firmware/GanJing65/readme.md
@@ -0,0 +1,19 @@
+# GanJing 70
+
+![GanJing 70](https://github.com/mingzejin11/GanJing-65-2.0/blob/main/Images/GanJing_65_rev_2.0_layout.png)
+
+A 70% keyboard that can be assembled with only through hole components, including usb type-c and the Elite-C V4
+
+* Keyboard Maintainer: [mingzejin11](https://github.com/mingzejin11)
+* Hardware Supported: GanJing 70, atmega32u4
+* Hardware Availability: [GitHub](https://github.com/mingzejin11/GanJing-65-2.0)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make mingzejin11/GanJing65:default
+
+Flashing example for this keyboard:
+
+    make mingzejin11/GanJing65:default:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
